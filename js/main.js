48c2c4acdf6fb169ae00819969adae5a3e1b0745
infokuj27-source <info.ku.j27@gmail.com>
2026-07-08 09:40:36 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+/**
+ * ==========================================================
+ * CPNS Learning Center
+ * Main JavaScript Entry Point
+ * ==========================================================
+ * Author  : Jayade Atafani
+ * Version : 1.0.0
+ * ==========================================================
+ */
 
+/* ===========================
+   Import Modules
+=========================== */
+
+import "./loader.js";
+import "./navbar.js";
+import "./mobile-menu.js";
+import "./darkmode.js";
+import "./scroll.js";
+import "./animation.js";
+import "./backtotop.js";
+import "./countdown.js";
+import "./progress.js";
+import "./counter.js";
+import "./faq.js";
+import "./quote.js";
+import "./search.js";
+
+/* ===========================
+   Website Initialization
+=========================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("=======================================");
+    console.log("🚀 CPNS Learning Center");
+    console.log("Version : 1.0.0");
+    console.log("Status  : Ready");
+    console.log("=======================================");
+
+});
