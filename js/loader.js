06996bda1515f6dfa512116228aa42aa872a76b8
infokuj27-source <info.ku.j27@gmail.com>
2026-07-08 09:49:04 +0800

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,35 @@
+/**
+ * ==========================================
+ * Loader Module
+ * ==========================================
+ */
 
+function initLoader() {
+
+    const loader = document.getElementById("loader");
+
+    if (!loader) {
+        return;
+    }
+
+    window.addEventListener("load", () => {
+
+        setTimeout(() => {
+
+            loader.style.opacity = "0";
+
+            loader.style.pointerEvents = "none";
+
+            setTimeout(() => {
+
+                loader.remove();
+
+            },600);
+
+        },800);
+
+    });
+
+}
+
+initLoader();
