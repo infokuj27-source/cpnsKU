b004ca14b6dfec102ba29d66890e70111fe58b4b
infokuj27-source <info.ku.j27@gmail.com>
2026-07-08 10:19:19 +0800

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,48 @@
+/**
+ * ==========================================================
+ * File      : scroll.js
+ * Module    : Smooth Scroll
+ * Project   : CPNS Learning Center
+ * Version   : 1.0.0
+ * ==========================================================
+ */
 
+export function initScroll() {
+
+    // Ambil semua link navigasi
+    const navLinks = document.querySelectorAll('a[href^="#"]');
+
+    // Validasi
+    if (!navLinks.length) return;
+
+    const NAVBAR_HEIGHT = 80;
+
+    navLinks.forEach(link => {
+
+        link.addEventListener("click", function(event){
+
+            const targetId = this.getAttribute("href");
+
+            const target = document.querySelector(targetId);
+
+            if(!target) return;
+
+            event.preventDefault();
+
+            const position =
+
+                target.offsetTop - NAVBAR_HEIGHT;
+
+            window.scrollTo({
+
+                top:position,
+
+                behavior:"smooth"
+
+            });
+
+        });
+
+    });
+
+}
