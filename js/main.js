8581bf5296804366db1360ea66e173df0ae8cede
infokuj27-source <info.ku.j27@gmail.com>
2026-08-03 14:12:28 +0800

    main upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+async function initWebsite() {
+
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+
+        ${createNavbar()}
+
+        ${createHero()}
+        ${createAnnouncement()}
+
+        ${createDashboard()}
+
+        ${await createMateri()}
+
+        ${await createSoal()}
+
+        ${await createInformasi()}
+
+        ${await createQuote()}
+        
+
+        ${await createFAQ()}
+        ${createFooter()}
+
+    `;
+    AOS.init({
+        duration: 800,
+        once: true
+    });
+
+    initFAQ();
+
+    initSearch();
+
+     initAssistant();
+
+    updateMateriStatus();
+
+    initCountdown();
+
+    initDarkMode();
+    initMobileMenu();
+
+    initBackToTop();
+
+}
+
+initWebsite();
